f2616a8a60e4181fd43e59934021c05a848de7ef
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-08-14 12:28:15 -0500

    Reorganizing some of the basic computing files.

--- tutorials/basic_computing_2/snippets/read_csv_from_url.R
+++
@@ -1,9 +0,0 @@
-# Download a .csv.gz file from the Internet, and import in into R as a
-# data frame.
-storm_event_data_url <-
-  paste("https://www.ncei.noaa.gov/pub/data/swdi/stormevents/csvfiles",
-        "StormEvents_details-ftp_v1.0_d1990_c20220425.csv.gz", sep = "/")
-filename <- tempfile()
-download.file(storm_event_data_url, filename)
-storm_event_data <- read.csv(filename, stringsAsFactors = FALSE)
-unlink(filename)
--- tutorials/basic_computing_2/snippets/reverse_complement.R
+++
@@ -1,4 +0,0 @@
-# Get the reverse complement of a DNA sequence.
-sequence <- "ACCTGAG"
-reversed <- paste(rev(unlist(strsplit(sequence, NULL))), collapse = "")
-rev_complement <- chartr("ATGCN", "TACGN", reversed)
